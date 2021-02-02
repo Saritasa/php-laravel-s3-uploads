283f515a01279a7373c43d3a2cc9b8bcc505000e
Sergey Populov <sergey@saritasa.com>
2021-02-02 18:37:48 +0500

    Allow cacheable presigned URLs

--- src/Traits/HasUploadedFile.php
+++ src/Traits/HasUploadedFile.php
@@ -2,22 +2,60 @@
 
 namespace Saritasa\LaravelUploads\Traits;
 
+use Carbon\Carbon;
+use DateTimeInterface;
 use Illuminate\Support\Facades\Storage;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
 
 trait HasUploadedFile
 {
     /**
-     * Convert image path on S3 bucket to pre-signed read URL
+     * Convert file path on S3 bucket to pre-signed read URL
      *
-     * @param string|null $imagePath Path inside S3 bucket
+     * @param string|null $filePath Path inside S3 bucket
      *
      * @return string|null
      */
-    public function fileUrl(?string $imagePath): ?string
+    public function fileUrl(?string $filePath): ?string
     {
-        if (!$imagePath || strpos($imagePath, '://') !== false) {
-            return $imagePath;
+        if (!$filePath || strpos($filePath, '://') !== false) {
+            return $filePath;
         }
-        return Storage::cloud()->temporaryUrl(trim($imagePath, '/'), config('media.expires'));
+        $cloud = Storage::cloud();
+
+        $adapter = $cloud->getAdapter();
+        if ($adapter instanceof AwsS3Adapter) {
+            return $this->getAwsTemporaryUrl($adapter, trim($filePath, '/'), config('media.expires'), [
+                'start_time' => Carbon::now()->setMinute(0)->setSeconds(0)->setMilliseconds(0),
+            ]);
+        } elseif (method_exists($cloud->getAdapter(), 'getTemporaryUrl')) {
+            return $cloud->temporaryUrl(trim($filePath, '/'), config('media.expires'));
+        } else {
+            return $cloud->url($filePath);
+        }
+    }
+
+    /**
+     * Fixed version of FilesystemAdapter->getAwsTemporaryUrl() method:
+     * original method doesn't pass [options] argument to $client->createPresignedRequest() method,
+     * thus, it is impossible to create cacheable presigned URLs
+     *
+     * @param AwsS3Adapter $adapter S3 Adapter
+     * @param string $path File path inside S3 bucket
+     * @param string|DateTimeInterface $expiration When link should become invalid
+     * @param array $options Other options (ex. when is "start_time" of expiration base)
+     *
+     * @return string
+     */
+    private function getAwsTemporaryUrl($adapter, $path, $expiration, $options)
+    {
+        $client = $adapter->getClient();
+
+        $command = $client->getCommand('GetObject', array_merge([
+            'Bucket' => $adapter->getBucket(),
+            'Key' => $adapter->getPathPrefix().$path,
+        ], $options));
+
+        return (string) $client->createPresignedRequest($command, $expiration, $options)->getUri();
     }
 }
